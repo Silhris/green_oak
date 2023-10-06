8a75d47580226e5b08bfb2f09cb185003adc9e7e
Alexandre Crépu <acrepu@positivethinking.tech>
2023-10-06 11:42:54 +0200

    wip

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { GalleriaModule } from 'primeng/galleria';
+import { TooltipModule } from 'primeng/tooltip';
+
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    GalleriaModule,
+    TooltipModule
+  ],
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.less']
 })
 export class GalleryComponent {
-
+  images: any[] = [
+    {
+      "previewImageSrc": "assets/images/_6d5dfa87-ed1a-4aaa-bcc3-6f3edc407033.jpg",
+      "thumbnailImageSrc": "assets/images/_6d5dfa87-ed1a-4aaa-bcc3-6f3edc407033.jpg",
+      "alt": "Une coopérative d'agriculteur de Green Oak Corporation",
+      "title": "Présentation de l'entreprise Green Oak Corporation"
+    },
+    {
+      "previewImageSrc": "assets/images/_886bb36c-3d21-460d-8b5c-ed79a783477d.jpg",
+      "thumbnailImageSrc": "assets/images/_886bb36c-3d21-460d-8b5c-ed79a783477d.jpg",
+      "alt": "Diverses graines vendus par Green Oak Corporation",
+      "title": "Produits Green Oak Corporation"
+    },
+    {
+      "previewImageSrc": "assets/images/_5081e0ca-7c1a-4c43-9d6a-271aac6eb205.jpg",
+      "thumbnailImageSrc": "assets/images/_5081e0ca-7c1a-4c43-9d6a-271aac6eb205.jpg",
+      "alt": "Diverses explications à propos des graines vendus par Green Oak Corporation",
+      "title": "Comment bien utiliser les graines de Green Oak Corporation"
+    },
+    {
+      "previewImageSrc": "assets/images/_a660e138-66f2-4246-b317-92e62a32c27a.jpg",
+      "thumbnailImageSrc": "assets/images/_a660e138-66f2-4246-b317-92e62a32c27a.jpg",
+      "alt": "Une planète entourée d'arbres et d'un homme entretenant son potager",
+      "title": "Conseils pour être un acteur éco-responsable"
+    },
+    {
+      "previewImageSrc": "assets/images/_aaf1123b-95e8-4d49-a82c-2865b2481fd8.jpg",
+      "thumbnailImageSrc": "assets/images/_aaf1123b-95e8-4d49-a82c-2865b2481fd8.jpg",
+      "alt": "Des graines vendus par Green Oak Corporation",
+      "title": "Explications à propos des graines vendus par Green Oak Corporation"
+    },
+    {
+      "previewImageSrc": "assets/images/_fcca6dbb-4e98-4932-8cea-9798d92a2395.jpg",
+      "thumbnailImageSrc": "assets/images/_fcca6dbb-4e98-4932-8cea-9798d92a2395.jpg",
+      "alt": "Des industries entourées d'arbres",
+      "title": "L'industrie 4.0 éco-responsable"
+    }
+  ];
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,5 +9,68 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./products.component.less']
 })
 export class ProductsComponent {
+  // TODO: faire un service pour récup les produits ?
 
+  plants_truffiers: any[] = [
+    { name: 'Chêne Vert Truffier Tuber Mélanosporum (quercus ilex)', image: 'assets/images/products/truffiers/chene-truffier-pubescent-tuber-melanosporum-quercus-pubescens.jpg', price: 15.95,
+      description: 'Jeune plant truffier de chêne pubescent (Quercus pubescens) aussi appelé chêne blanc mycorhizé par tuber mélanosporum de 2 ans en godets anti-chignon de 450cc. Contrôlé et certifié par le CTIFL.' },
+    { name: 'Chêne Truffier Pubescent Tuber Mélanosporum (quercus pubescens)', image: 'assets/images/truffiers/products/chene-vert-truffier-tuber-melanosporum-quercus-ilex.jpg', price: 24.99,
+      description: 'Jeune plant truffier de chêne vert (Quercus ilex) mycorhizé par tuber mélanosporum de 2 ans en godets anti-chignon de 450cc. Contrôlé et certifié par le CTIFL.'},
+    { name: 'Noisetier Commun Truffier Tuber Mélanosporum (corylus avellana)', image: 'assets/images/truffiers/products/noisetier-commun-truffier-tuber-melanosporum-corylus-avellana.jpg', price: 15.95,
+      description: 'Jeune plant truffier de noisetier commun (corylus avellana) mycorhizé par tuber mélanosporum de 2 ans en godets anti-chignon de 450cc. Contrôlé et certifié par le CTIFL.' },
+    { name: 'Chêne Truffier Kermes Tuber Mélanosporum (quercus coccifera)', image: 'assets/images/products/truffiers/chene-truffier-kermes-tuber-melanosporum-quercus-coccifera.jpg', price: 15.95,
+      description: 'Jeune plant truffier de chêne kermès (Quercus coccifera) ou chêne des garrigues mycorhizé par tuber mélanosporum de 2 ans en godets anti-chignon de 450cc. Contrôlé et certifié par le CTIFL.' },
+    { name: 'Charme Truffier Tuber Mélanosporum (carpinus betulus)', image: 'assets/images/products/truffiers/charme-truffier-tuber-melanosporum-carpinus-betulus.jpg', price: 24.99,
+      description: 'Jeune plant truffier de charme truffier (carpinus betulus) mycorhizé par tuber mélanosporum de 2 ans en godets anti-chignon de 450cc. Contrôlé et certifié par le CTIFL.' },
+    { name: 'Noisetier Truffier Byzance Tuber Mélanosporum (corylus colurna)', image: 'assets/images/products/truffiers/noisetier-truffier-byzance-tuber-melanosporum-corylus-colurna.jpg', price: 15.95,
+      description: '' },
+    { name: 'Pack de Plants Truffiers spécial Uncinatum', image: 'assets/images/products/truffiers/pack-de-plants-truffiers-special-uncinatum.jpg', price: 154.00,
+      description: 'NOUVEAU ! On vous propose désormais des packs truffiers spécial Uncinatum avec une dose d\'engrais retard spécifique (type basacote) pour chaque plants.' }
+  ];
+
+  coniferes: any[] = [
+    { name: 'Arbre aux 40 écus', image: 'assets/images/products/coniferes/arbre-aux-40-ecus17922.jpg', price: 26.50,
+      description: 'Gingko biloba est un arbre de très grande longévité (plus de 1 000 ans). Son port d\'abord dressé, s\'étale à partir de l\'âge de 20 ans. Les feuilles caduques, en éventail, à long pétiole, vert vif, prennent une magnifique couleur jaune d\'or en automne.' },
+    { name: 'Arbre aux 40 écus Eiffel', image: 'assets/images/products/coniferes/arbre-aux-40-ecus-eiffel-23076.jpg', price: 245.00,
+      description: 'Gingko fastigié, ventru à la base, rétréci au sommet. Superbes couleurs d\'automne, comme tous les Ginkgo.' },
+    { name: 'Arbre aux 40 écus Fastigiata Blagon', image: 'assets/images/products/coniferes/arbre-aux-40-ecus-fastigiata-blagon-4814.jpg', price: 69.00,
+      description: 'Une nouvelle forme colonnaire, compacte et mâle (pas de fruits malodorant) de l\'arbre au 40 écus. Croissance lente, entre-nœuds courts.' },
+    { name: 'Cèdre bleu de l\'Atlas', image: 'assets/images/products/coniferes/cedre-bleu-de-l-atlas17825.jpg', price: 129.00,
+      description: 'Conifère conique à feuillage bleu très vif, surtout au printemps. En automne, cônes dressés cylindriques. Majestueux.' },
+    { name: 'Cèdre du Japon Green Pearl', image: 'assets/images/products/coniferes/cedre-du-japon-green-pearl-22973.jpg', price: 16.90,
+      description: 'Conifère nain, formant une boule et à feuillage très dense aux nouvelles pousses vert tendre, puis vert sombre, virant au bronze en hiver.' }
+  ];
+
+  quercus: any[] = [
+    { name: 'Chêne vert', image: 'assets/images/products/quercus/quercus-ilex-chene-vert-chene-faux-houx-yeuse.jpg', price: ,
+      description: 'Le Quercus ilex est originaire d\'Europe méridionale et d\'Afrique du Nord. Son feuillage vert persistant lui vaut le nom de Chêne vert et la ressemblance des feuilles avec les feuilles de houx commun (Ilex aquifolium) lui a donné ce nom vernaculaire de Chêne faux-houx.' },
+    { name: 'Chêne rouge \'Amérique', image: 'assets/images/products/quercus/quercus-rubra-chene-rouge-d-amerique-en-godet-forestier.jpg', price: 6.99,
+      description: 'Le chêne rouge d\'Amérique (Quercus rubra L., syn. Quercus borealis Duroi) est un arbre appartenant à la section des chênes rouges (Lobatae) du genre Quercus de la famille des Fagacées1. Il s\'agit de l\'arbre symbole de l\'État du New Jersey et de l’Île-du-Prince-Édouard.' },
+    { name: 'Chêne des marais', image: 'assets/images/products/quercus/quercus-palustris-chene-des-marais.jpg', price: 4.99,
+      description: 'Le Chêne des marais ou Chêne à épingles (Quercus palustris) est un arbre de la famille des Fagaceae originaire de l\'Est des États-Unis d\'Amérique et du Canada' },
+    { name: 'Chêne bambou', image: 'assets/images/products/quercus/quercus-myrsinifolia-chene-bambou-a-feuilles-de-myrsine.jpg', price: 9.65,
+      description: 'Un arbuste rare, venu du pays du soleil levant. Pyramidal dans sa jeunesse, il prend son temps pour former une couronne arrondie, et déploie un feuillage élégant et mince, bronze au débourrement, vert en été, devenant pourpre en automne, persistant en hiver.' },
+    { name: 'Chêne asiatique', image: 'assets/images/products/quercus/quercus-phillyreoides-chene-a-feuilles-de-filaire-persistant.jpg', price: 8.86,
+      description: 'Petit arbre globuleux qui fait aussi de jolies haies. Petites feuilles assez rondes, coriaces et épaisses, de couleur rouge orangé au débourrement. Il résiste très bien au froid ainsi qu\'à la sécheresse.' }
+  ];
+
+  arbres_fruitiers: any[] = [
+    { name: 'Abricotier Bergeron', image: 'assets/images/products/arbres_fruitiers/abricotier_bergeron_m.jpg', price: 22.90,
+      description: 'Abricotier produisant des fruits d\'une couleur safran caractéristique. La chair est ferme et parfumée. Cette variété d\'abricotier forme des arbres vigoureux, très productifs et très résistants au gel. La récolte a lieu début août. ' },
+    { name: 'Cerisier Bigarreau Saint Antoine®', image: 'assets/images/products/arbres_fruitiers/Cerisier_Bigarreau_Saint_Antoine_Poisdel_m.jpg', price: 21.90,
+      description: 'Ce cerisier propose de gros fruits rouges sucrés en forme de cœur. Il offre une bonne production de fruits charnus et parfumés. Ses cerises se récoltent aux alentours du 13 juin, jour de la Saint Antoine de Padoue.' },
+    { name: 'Collection Les Nectarines', image: 'assets/images/products/arbres_fruitiers/collection_les_nectarines_m.jpg', price: 67.50,
+      description: 'Le nectarinier Nectarine Sanguine d´Auvergne donne de beaux fruits ronds à la peau rouge cardinal striée et à la chair rouge pourpre veinée de blanc, juteuse et fondante. C\'est une variété qui résiste très bien à la cloque.' },
+    { name: 'Collection Les fruits d´été', image: 'assets/images/products/arbres_fruitiers/collection_les_fruits_d_ete_m.jpg', price: 104.90,
+      description: 'L\'abricotier Rouge Tardif Delbard® vous donnera des fruits qui ont un bon équilibre entre le sucre et l´acidité et à la chair moelleuse et fondante.' },
+    { name: 'Prunier Mirabelle de Metz', image: 'assets/images/products/arbres_fruitiers/prunier_mirabelle_de_metz_m.jpg', price: 31.90,
+      description: 'Ce prunier produit de petits fruits jaune doré nuancé de rouge côté soleil. La chair délicieusement parfumée est très fine et très sucrée.' },
+    { name: 'Pluot Flavor Suprême®', image: 'assets/images/products/arbres_fruitiers/Pluot_FlavorSupreme_m.jpg', price: 39.90,
+      description: 'Gros fruit de la taille d\'une pêche au coloris vert-gris, à la chair, sanguine, juteuse,sucrée et parfumée. Délicieux en fruits frais, ils peuvent être utilisés en patisseries et en confitures. Pluot® a été obtenu par croisement de prune et d\'abricot.' },
+  ];
+
+  arbustes_fruitiers: any[] = [
+    { name: '', image: 'assets/images/products/arbrustes_fruitiers/', price: ,
+      description: '' }
+  ];
 }
